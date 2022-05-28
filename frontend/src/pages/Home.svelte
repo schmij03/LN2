f97338e4670e17a5f3a44fc12e983c1a8861ee82
schmij03 <schmij03@students.zhaw.ch>
2022-05-28 16:45:49 +0200

    commit

---
+++ frontend/src/pages/Home.svelte
@@ -0,0 +1,43 @@
+<div class="my-5" style="text-align: center" />
+<h1 class="mt-12">Ihr Grümpi Organisator</h1>
+<br />
+<br />
+<br />
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4 mb-1">
+            <div class="card h-100">
+                <img src="/images/players.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Players</h5>
+                    <p class="card-text">Overview of Players</p>
+                    <a href="#/players" class="btn btn-primary">Go to Players</a
+                    >
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4 mb-1">
+            <div class="card h-100">
+                <img src="/images/team.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Teams</h5>
+                    <p class="card-text">Overview of Teams</p>
+                    <a href="#/teams" class="btn btn-primary">Go to Teams</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4 mb-1">
+            <div class="card h-100">
+                <img src="/images/events.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Events</h5>
+                    <p class="card-text">Overview of Events</p>
+                    <a href="#/events" class="btn btn-primary">Go to Events</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ frontend/src/pages/Players/Players.svelte
@@ -0,0 +1,56 @@
+<script>
+    import axios from "axios";
+    
+    let players = [];
+
+    function getPlayers() {
+        axios.get("http://localhost:3001/api/players").then((response) => {
+            players = response.data;
+        });
+    }
+    getPlayers();
+
+  
+
+</script>
+
+<div class="mb-5">
+    <h1 class="mt-3">List of all Players</h1>
+    
+    <a href="#/create-player"><button type="button" class="btn btn-primary">Add Player </button></a>
+
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Birthdate</th>
+                <th>Player Details</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each players as player}
+                <tr>
+                    <td>
+                        {player.name}
+                        
+                    </td>
+                        <td>
+                        {player.gender}
+                        </td>
+                    <td>
+                        {player.birthdate}
+                    </td>
+
+                    <td>
+                        <a href={"#/players/" + player._id}>
+                            Details
+                        </a>
+                    </td>
+
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
